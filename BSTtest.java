45a39c565982a554f492140409e18ab77d63b538
darrenhuang22 <darrenhuang22@gmail.com>
2014-07-24 18:44:09 -0400

    Fist

---
+++ BSTtest.java
@@ -0,0 +1,234 @@
+import java.util.*;
+
+class BSTtest
+{
+        static public void main(String[] args)
+        {
+                MyBST mbst = new MyBST();
+                int[] keys = new int[]{15,6,18,3,7,13,20,2,9,4};
+
+                for(int i=0;i<keys.length;i++)
+                {
+                        mbst.insert(keys[i]);
+                }
+                // First print out the root key value
+                System.out.print("(1) let's print out the root key: ");
+                mbst.printKey(mbst.getRoot());
+                
+                // Second print out the searched key value
+                int key = 210;
+                System.out.print("(2) let's see if we can find "+key+": ");
+                mbst.printKey(mbst.search(mbst.getRoot(),key));
+                
+                // Third print out the minimum key value
+                System.out.print("(3) let's see the minimum key: ");
+                mbst.printKey(mbst.min(mbst.getRoot()));
+
+                // Fourth print out the maximum key value
+                System.out.print("(4) let's see the maximum key: ");
+                mbst.printKey(mbst.max(mbst.getRoot()));
+
+                // Fifth print out the successor key value
+                System.out.print("(5) let's see " + 13 + " successor key: ");
+                mbst.printKey(mbst.successor(mbst.search(mbst.getRoot(),13)));
+
+                // Sixth print out the succssor key value
+                System.out.print("(6) let's see " + 7 + " predecessor key: ");
+                mbst.printKey(mbst.predecessor(mbst.search(mbst.getRoot(),7)));
+
+                // Seventh, let's delete something
+                System.out.print("(7) let's delete " + 7 + " :");
+                mbst.delete(7);
+        }
+}
+
+
+class MyBST
+{
+        private Node root = null;
+        private class Node{
+                private int key;
+                private Node left;
+                private Node right;
+                private Node p;
+        
+                public Node(int key, Node left, Node right, Node p)
+                {
+                        this.key = key;
+                        this.left = left;
+                        this.right = right;
+                        this.p = p;
+                }
+
+        }
+
+        public void printKey(Node x)
+        {
+                if (x!=null)
+                {
+                        System.out.println(x.key);
+                }else
+                {
+                        System.out.println("null, sb!!");
+                }
+        }
+
+        public Node getRoot()
+        {
+                return root;
+        }
+        
+        public Node search(Node x, int key)
+        {
+                if (x==null || key == x.key)
+                {
+                        return x;
+                }else if (key < x.key)
+                {
+                        return search(x.left,key);
+                }else
+                {
+                        return search(x.right,key);
+                }
+        }
+
+        public Node min(Node x)
+        {
+                while(x.left != null)
+                        x = x.left;
+                return x;
+        }
+
+        public Node max(Node x)
+        {
+                while(x.right != null)
+                        x = x.right;
+                return x;
+        }
+
+        public Node successor(Node x)
+        {
+                if (x == null)
+                        return x;
+                if (x.right!=null)
+                {
+                        return min(x.right);
+                }
+                Node y = x.p;
+                while(y!=null && x == y.right)
+                {
+                        x = y;
+                        y = y.p;
+                }
+                return y;
+        }
+
+        public Node predecessor(Node x)
+        {
+                if (x == null)
+                        return x;
+                if (x.left != null)
+                {
+                        return max(x.left);
+                }
+                Node y = x.p;
+                while(y != null && x == y.left)
+                {
+                        x = y;
+                        y = y.p;
+                }
+                return y;
+        }
+
+
+        public void insert(int key)
+        {
+                Node y = null;
+                Node x = root;
+                Node z = new Node(key, null, null, null);
+                while(x!=null)
+                {
+                        y = x;
+                        if (z.key < x.key)
+                        {
+                                x = x.left;
+                        }else
+                        {
+                                x = x.right;
+                        }
+                }
+                z.p = y;
+                if (y == null)
+                {
+                        root = z;
+                }else if (z.key < y.key)
+                {
+                        y.left = z;
+                }else
+                {
+                        y.right = z;
+                }
+        }
+
+        private void transplant(Node u, Node v)
+        {
+                if (u.p == null)
+                {
+                        root = v;
+                }
+                else if (u == u.p.left)
+                {
+                        u.p.left = v;
+                }else
+                {
+                        u.p.right = v;
+                }
+                if (v != null)
+                        v.p = u.p;
+        }
+
+        private void delete(Node z)
+        {
+                if (z.left == null)
+                {
+                        transplant(z,z.right);
+                }else if (z.right == null)
+                {
+                        transplant(z,z.left);
+                }else
+                {
+                        Node y = min(z.right);
+                        if (y.p != z)
+                        {
+                                transplant(y,y.right);
+                                y.right = z.right;
+                                y.right.p = y;
+                        }
+                        transplant(z,y);
+                        y.left = z.left;
+                        y.left.p = y;
+                }
+        }
+
+        public void delete(int key)
+        {
+                Node z = search(root,key);
+                if (z == null)
+                {
+                        System.out.println("SB, i can not find it");
+                }else
+                {
+                        delete(z);
+                }
+        }
+        public void inorderTreeWalk(Node x)
+        {
+                if (x!=null)
+                {
+                        inorderTreeWalk(x.left);
+                        printKey(x);
+                        inorderTreeWalk(x.right);
+                }
+        }
+
+}
---
+++ RBtest.java
@@ -0,0 +1,330 @@
+class RBtest
+{
+        public static void main(String[] args)
+        {
+                System.out.println("Trying to implement Red-Black Tree (Cao): ");
+                //String s = "Black";
+                //System.out.println(s.equals("Black")?"Black":"Red");
+
+                RBTree rbt = new RBTree();
+                int[] keys = new int[]{15,6,18,3,7,13,20,2,9,4};
+
+                for(int i=0;i<keys.length;i++)
+                        rbt.RBinsert(keys[i]);
+                //rbt.printKey(rbt.getRoot());
+                rbt.inorderTreeWalk(rbt.getRoot());
+
+                //rbt.printKey(rbt.search(rbt.getRoot(),7));
+
+                //rbt.printKey(rbt.search(rbt.getRoot(),111));
+
+                System.out.println("Let's delete:");
+                rbt.delete(60);
+
+                rbt.inorderTreeWalk(rbt.getRoot());
+        }
+}
+
+class RBTree
+{
+        private class Node{
+                private int key;
+                private String color;
+                Node left;
+                Node right;
+                Node p;
+
+                public Node(int key, String color, Node left, Node right, Node p)
+                {
+                        this.key = key;
+                        this.color = color;
+                        this.left = left;
+                        this.right = right;
+                        this.p = p;
+                }
+        }
+
+        private Node Tnill = new Node(-100,"Black",null,null,null);
+
+        private Node root = Tnill;
+
+        public void printKey(Node x)
+        {
+                if (x == Tnill)
+                        System.out.println("SB, It's empty!!");
+                else
+                        System.out.println("Key: "+x.key+"("+x.color+"). Left: "+x.left.key+". Right: "+x.right.key+".");
+        }
+
+        public Node getRoot()
+        {
+                return root;
+        }
+
+        public void inorderTreeWalk(Node x)
+        {
+                if (x != Tnill)
+                {
+                        inorderTreeWalk(x.left);
+                        printKey(x);
+                        inorderTreeWalk(x.right);
+                }
+        }
+
+        public Node search(Node x, int key)
+        {
+                if (x.key == key || x == Tnill)
+                        return x;
+                else if (x.key < key)
+                        return search(x.right, key);
+                else
+                        return search(x.left, key);
+        }
+
+        public Node min(Node x)
+        {
+                while (x.left != Tnill)
+                        x = x.left;
+                return x;
+        }
+
+        public Node max(Node x)
+        {
+                while (x.right != Tnill)
+                        x = x.right;
+                return x;
+        }
+
+        private void leftRotate(Node x)
+        {
+                Node y = x.right;
+                x.right = y.left;
+                if (y.left != Tnill)
+                        y.left.p = x;
+                y.p = x.p;
+                if (x.p == Tnill)
+                        root = y;
+                else if (x.p.left == x)
+                        x.p.left = y;
+                else
+                        x.p.right = y;
+                y.left = x;
+                x.p = y;
+        }
+
+        private void rightRotate(Node y)
+        {
+                Node x = y.left;
+                y.left = x.right;
+                if (x.right != Tnill)
+                        x.right.p = y;
+                x.p = y.p;
+                if (y.p == Tnill)
+                        root = x;
+                else if (y.p.left == y)
+                        y.p.left = x;
+                else
+                        y.p.right = x;
+                x.right = y;
+                y.p = x;
+        }
+
+        public void RBinsert(int key)
+        {
+                Node y = Tnill;
+                Node x = root;
+                Node z = new Node(key,"RED",Tnill,Tnill,Tnill);
+                while (x != Tnill)
+                {
+                        y = x;
+                        if (z.key < x.key)
+                                x = x.left;
+                        else
+                                x = x.right;
+                }
+                z.p = y;
+                if (y == Tnill)
+                        root = z;
+                else if (z.key < y.key)
+                        y.left = z;
+                else
+                        y.right = z;
+                RBinsertFix(z);
+        }
+
+        private void RBinsertFix(Node z)
+        {
+                while(z.p.color.equals("RED"))
+                {
+                        if (z.p == z.p.p.left)
+                        {
+                                Node y = z.p.p.right; // Uncle
+                                if (y.color.equals("RED"))
+                                {
+                                        z.p.color = "BLACK";
+                                        y.color = "BLACK";
+                                        z.p.p.color = "RED";
+                                        z = z.p.p;
+                                }
+                                else 
+                                {
+                                        if (z == z.p.right)
+                                        {
+                                                z = z.p;
+                                                leftRotate(z);
+                                        }
+                                        z.p.color = "BLACK";
+                                        z.p.p.color = "RED";
+                                        rightRotate(z.p.p);
+                                }
+                        }else
+                        {
+                                Node y = z.p.p.left; // Uncle
+                                if (y.color.equals("RED"))
+                                {
+                                        z.p.color = "BLACK";
+                                        y.color = "BLACK";
+                                        z.p.p.color = "RED";
+                                        z = z.p.p;
+                                }
+                                else 
+                                {
+                                        if (z == z.p.left)
+                                        {
+                                                z = z.p;
+                                                rightRotate(z);
+                                        }
+                                        z.p.color = "BLACK";
+                                        z.p.p.color = "RED";
+                                        leftRotate(z.p.p);
+                                }
+                        }
+                }
+                root.color = "BLACK";
+        }
+
+        private void RBtransplant(Node u, Node v)
+        {
+                if (u.p == Tnill)
+                        root = v;
+                else if (u.p.left == u)
+                        u.p.left = v;
+                else
+                        u.p.right = v;
+                v.p = u.p;
+        }
+
+        public void delete(int key)
+        {
+                Node x = search(root, key);
+                if (x == Tnill)
+                        System.out.println("Sorry, can not delete since the tree does not contain this node, dumbass");
+                else
+                        delete(x);
+        }
+        
+        private void delete(Node z)
+        {
+                Node y = z;
+                Node x;
+                String y_o_color = y.color;
+                if (z.left == Tnill)
+                {
+                        x = z.right;
+                        RBtransplant(z, z.right);
+                }else if (z.right == Tnill)
+                {
+                        x = z.left;
+                        RBtransplant(z, z.left);
+                }else
+                {
+                        y = min(z.right);
+                        y_o_color = y.color;
+                        x = y.right;
+                        if (y.p == z)
+                                x.p = y;
+                        else
+                        {
+                                RBtransplant(y, y.right);
+                                y.right = z.right;
+                                y.right.p = y;
+                        }
+                        RBtransplant(z, y);
+                        y.left = z.left;
+                        y.left.p = y;
+                        y.color = z.color;
+                }
+                if (y_o_color.equals("BLACK"))
+                        RBdeleteFix(x);
+        }
+
+        private void RBdeleteFix(Node x)
+        {
+                while ( x != Tnill && x.color.equals("BLACK"))
+                {
+                        if (x == x.p.left)
+                        {
+                                Node w = x.p.right;
+                                if (w.color.equals("RED"))
+                                {
+                                        w.color = "BLACK";
+                                        x.p.color = "RED";
+                                        leftRotate(x.p);
+                                        w = x.p.right;
+                                }
+                                if (w.left.color.equals("BLACK") && w.right.color.equals("BLACK"))
+                                {
+                                        w.color = "RED";
+                                        x = x.p;
+                                }
+                                else 
+                                {
+                                        if (w.right.color.equals("BLACK"))
+                                        {
+                                                w.left.color = "BLACK";
+                                                w.color = "RED";
+                                                rightRotate(w);
+                                                w = x.p.right;
+                                        }
+                                        w.color = x.p.color;
+                                        x.p.color = "BLACK";
+                                        w.right.color = "BLACK";
+                                        leftRotate(x.p);
+                                        x = root;
+                                }
+                        }
+                        else
+                        {
+                                Node w = x.p.left;
+                                if (w.color.equals("RED"))
+                                {
+                                        w.color = "BLACK";
+                                        x.p.color = "RED";
+                                        rightRotate(x.p);
+                                        w = x.p.left;
+                                }
+                                if (w.left.color.equals("BLACK") && w.right.color.equals("BLACK"))
+                                {
+                                        w.color = "RED";
+                                        x = x.p;
+                                }
+                                else 
+                                {
+                                        if (w.left.color.equals("BLACK"))
+                                        {
+                                                w.right.color = "BLACK";
+                                                w.color = "RED";
+                                                leftRotate(w);
+                                                w = x.p.left;
+                                        }
+                                        w.color = x.p.color;
+                                        x.p.color = "BLACK";
+                                        w.left.color = "BLACK";
+                                        rightRotate(x.p);
+                                        x = root;
+                                }
+                        }
+                        x.color = "BLACK";
+                }
+        }
+}
