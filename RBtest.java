9e4c8006385fab86ab2538f9d68f5f693dcf6220
darrenhuang22 <darrenhuang22@gmail.com>
2014-08-26 18:52:26 -0400

    Yinjie Huang

--- RBtest.java
+++ RBtest.java
@@ -4,10 +4,8 @@
 // Date: 07-24-2014
 
 
-class Test
-{
-        public static void main(String[] args)
-        {
+class Test {
+        public static void main(String[] args) {
                 System.out.println("Trying to implement Red-Black Tree (Cao): ");
                 //String s = "Black";
                 //System.out.println(s.equals("Black")?"Black":"Red");
@@ -15,8 +13,9 @@ class Test
                 RBTree rbt = new RBTree();
                 int[] keys = new int[]{41,38,31,12,19,8};
 
-                for(int i=0;i<keys.length;i++)
+                for (int i=0;i<keys.length;i++) {
                         rbt.RBinsert(keys[i]);
+                }
                 //rbt.printKey(rbt.getRoot());
                 rbt.inorderTreeWalk(rbt.getRoot());
 
@@ -34,18 +33,16 @@ class Test
         }
 }
 
-class RBTree
-{
+class RBTree {
         // Construct Red-Black Tree
-        private class Node{
+        private class Node {
                 private int key;
                 private String color;
                 private Node left;
                 private Node right;
                 private Node p;
 
-                public Node(int key, String color, Node left, Node right, Node p)
-                {
+                public Node(int key, String color, Node left, Node right, Node p) {
                         this.key = key;
                         this.color = color;
                         this.left = left;
@@ -59,130 +56,122 @@ class RBTree
 
         private Node root = Tnill;
 
-        public void printKey(Node x)
-        {
-                if (x == Tnill)
+        public void printKey(Node x) {
+                if (x == Tnill) {
                         System.out.println("It's empty!!");
-                else
+                } else {
                         System.out.println("Key: "+x.key+"("+x.color+"). Left: "+x.left.key+". Right: "+x.right.key+".");
+                }
         }
 
-        public Node getRoot()
-        {
+        public Node getRoot() {
                 return root;
         }
 
-        public void inorderTreeWalk(Node x)
-        {
-                if (x != Tnill)
-                {
+        public void inorderTreeWalk(Node x) {
+                if (x != Tnill) {
                         inorderTreeWalk(x.left);
                         printKey(x);
                         inorderTreeWalk(x.right);
                 }
         }
 
-        public Node search(Node x, int key)
-        {
-                if (x.key == key || x == Tnill)
+        public Node search(Node x, int key) {
+                if (x.key == key || x == Tnill) {
                         return x;
-                else if (x.key < key)
+                } else if (x.key < key) {
                         return search(x.right, key);
-                else
+                } else {
                         return search(x.left, key);
+                }
         }
 
-        public Node min(Node x)
-        {
-                while (x.left != Tnill)
+        public Node min(Node x) {
+                while (x.left != Tnill) {
                         x = x.left;
+                }
                 return x;
         }
 
-        public Node max(Node x)
-        {
-                while (x.right != Tnill)
+        public Node max(Node x) {
+                while (x.right != Tnill) {
                         x = x.right;
+                }
                 return x;
         }
 
         // Left Rotate
-        private void leftRotate(Node x)
-        {
+        private void leftRotate(Node x) {
                 Node y = x.right;
                 x.right = y.left;
-                if (y.left != Tnill)
+                if (y.left != Tnill) {
                         y.left.p = x;
+                }
                 y.p = x.p;
-                if (x.p == Tnill)
+                if (x.p == Tnill) {
                         root = y;
-                else if (x.p.left == x)
+                } else if (x.p.left == x) {
                         x.p.left = y;
-                else
+                } else {
                         x.p.right = y;
+                }
                 y.left = x;
                 x.p = y;
         }
 
-        private void rightRotate(Node y)
-        {
+        private void rightRotate(Node y) {
                 Node x = y.left;
                 y.left = x.right;
-                if (x.right != Tnill)
+                if (x.right != Tnill) {
                         x.right.p = y;
+                }
                 x.p = y.p;
-                if (y.p == Tnill)
+                if (y.p == Tnill) {
                         root = x;
-                else if (y.p.left == y)
+                } else if (y.p.left == y) {
                         y.p.left = x;
-                else
+                } else {
                         y.p.right = x;
+                }
                 x.right = y;
                 y.p = x;
         }
 
-        public void RBinsert(int key)
-        {
+        public void RBinsert(int key) {
                 Node y = Tnill;
                 Node x = root;
                 Node z = new Node(key,"RED",Tnill,Tnill,Tnill);
-                while (x != Tnill)
-                {
+                while (x != Tnill) {
                         y = x;
-                        if (z.key < x.key)
+                        if (z.key < x.key) {
                                 x = x.left;
-                        else
+                        } else {
                                 x = x.right;
+                        }
                 }
                 z.p = y;
-                if (y == Tnill)
+                if (y == Tnill) {
                         root = z;
-                else if (z.key < y.key)
+                } else if (z.key < y.key) {
                         y.left = z;
-                else
+                } else {
                         y.right = z;
+                }
                 RBinsertFix(z);
         }
 
         // Fix the tree after insertion, complated, please read Ch 13 in the Reference book
-        private void RBinsertFix(Node z)
-        {
-                while(z.p.color.equals("RED"))
-                {
-                        if (z.p == z.p.p.left)
-                        {
+        private void RBinsertFix(Node z) {
+                while(z.p.color.equals("RED")) {
+                        if (z.p == z.p.p.left) {
                                 Node y = z.p.p.right; // Uncle
-                                if (y.color.equals("RED"))
-                                {
+                                if (y.color.equals("RED")) {
                                         z.p.color = "BLACK";
                                         y.color = "BLACK";
                                         z.p.p.color = "RED";
                                         z = z.p.p;
-                                }
-                                else 
-                                {
-                                        if (z == z.p.right)
-                                        {
+                                } else {
+                                        if (z == z.p.right) {
                                                 z = z.p;
                                                 leftRotate(z);
                                         }
@@ -190,20 +179,15 @@ class RBTree
                                         z.p.p.color = "RED";
                                         rightRotate(z.p.p);
                                 }
-                        }else
-                        {
+                        } else {
                                 Node y = z.p.p.left; // Uncle
-                                if (y.color.equals("RED"))
-                                {
+                                if (y.color.equals("RED")) {
                                         z.p.color = "BLACK";
                                         y.color = "BLACK";
                                         z.p.p.color = "RED";
                                         z = z.p.p;
-                                }
-                                else 
-                                {
-                                        if (z == z.p.left)
-                                        {
+                                } else {
+                                        if (z == z.p.left) {
                                                 z = z.p;
                                                 rightRotate(z);
                                         }
@@ -216,49 +200,44 @@ class RBTree
                 root.color = "BLACK";
         }
 
-        private void RBtransplant(Node u, Node v)
-        {
-                if (u.p == Tnill)
+        private void RBtransplant(Node u, Node v) {
+                if (u.p == Tnill) {
                         root = v;
-                else if (u.p.left == u)
+                } else if (u.p.left == u) {
                         u.p.left = v;
-                else
+                } else {
                         u.p.right = v;
+                }
                 v.p = u.p;
         }
 
-        public void delete(int key)
-        {
+        public void delete(int key) {
                 Node x = search(root, key);
-                if (x == Tnill)
+                if (x == Tnill) {
                         System.out.println("Sorry, can not delete since the tree does not contain this node, dumbass");
-                else
+                } else {
                         delete(x);
+                }
         }
         
         // Delete a node in the tree
-        private void delete(Node z)
-        {
+        private void delete(Node z) {
                 Node y = z;
                 Node x;
                 String y_o_color = y.color;
-                if (z.left == Tnill)
-                {
+                if (z.left == Tnill) {
                         x = z.right;
                         RBtransplant(z, z.right);
-                }else if (z.right == Tnill)
-                {
+                } else if (z.right == Tnill) {
                         x = z.left;
                         RBtransplant(z, z.left);
-                }else
-                {
+                } else {
                         y = min(z.right);
                         y_o_color = y.color;
                         x = y.right;
-                        if (y.p == z)
+                        if (y.p == z) {
                                 x.p = y;
-                        else
-                        {
+                        } else {
                                 RBtransplant(y, y.right);
                                 y.right = z.right;
                                 y.right.p = y;
@@ -268,34 +247,27 @@ class RBTree
                         y.left.p = y;
                         y.color = z.color;
                 }
-                if (y_o_color.equals("BLACK"))
+                if (y_o_color.equals("BLACK")) {
                         RBdeleteFix(x);
+                }
         }
 
         // Fix the Red-Black property, please read Ch 13 from <Introduction to Algorithm>
-        private void RBdeleteFix(Node x)
-        {
-                while (x != root && x.color.equals("BLACK"))
-                {
-                        if (x == x.p.left)
-                        {
+        private void RBdeleteFix(Node x) {
+                while (x != root && x.color.equals("BLACK")) {
+                        if (x == x.p.left) {
                                 Node w = x.p.right;
-                                if (w.color.equals("RED"))
-                                {
+                                if (w.color.equals("RED")) {
                                         w.color = "BLACK";
                                         x.p.color = "RED";
                                         leftRotate(x.p);
                                         w = x.p.right;
                                 }
-                                if (w.left.color.equals("BLACK") && w.right.color.equals("BLACK"))
-                                {
+                                if (w.left.color.equals("BLACK") && w.right.color.equals("BLACK")) {
                                         w.color = "RED";
                                         x = x.p;
-                                }
-                                else 
-                                {
-                                        if (w.right.color.equals("BLACK"))
-                                        {
+                                } else {
+                                        if (w.right.color.equals("BLACK")) {
                                                 w.left.color = "BLACK";
                                                 w.color = "RED";
                                                 rightRotate(w);
@@ -307,26 +279,19 @@ class RBTree
                                         leftRotate(x.p);
                                         x = root;
                                 }
-                        }
-                        else
-                        {
+                        } else {
                                 Node w = x.p.left;
-                                if (w.color.equals("RED"))
-                                {
+                                if (w.color.equals("RED")) {
                                         w.color = "BLACK";
                                         x.p.color = "RED";
                                         rightRotate(x.p);
                                         w = x.p.left;
                                 }
-                                if (w.left.color.equals("BLACK") && w.right.color.equals("BLACK"))
-                                {
+                                if (w.left.color.equals("BLACK") && w.right.color.equals("BLACK")) {
                                         w.color = "RED";
                                         x = x.p;
-                                }
-                                else 
-                                {
-                                        if (w.left.color.equals("BLACK"))
-                                        {
+                                } else {
+                                        if (w.left.color.equals("BLACK")) {
                                                 w.right.color = "BLACK";
                                                 w.color = "RED";
                                                 leftRotate(w);
